0b0916db481213b2cf58408f2fc2f79cd86f6030
senecursos <cursos.sene@gmail.com>
2026-09-04 14:07:34 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'portugues-estrategico-cache-v1';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/estrategia.jpg',
+  '/imagem1.jpg',
+  '/Imagem2.jpg',
+  '/imagem3.jpg',
+  '/logoescale.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
